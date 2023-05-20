233fd45c83e985e3842a49e3f26dbc7824917a84
AllenLVieira <allenvieira96@gmail.com>
2023-05-20 13:13:27 -0300

    fix: improve code and method name
    - Fix method name
    - More organization

--- src/main/java/br/com/allen/utils/CSVUtils.java
+++ src/main/java/br/com/allen/utils/CSVUtils.java
@@ -14,24 +14,27 @@ import java.util.List;
 
 public class CSVUtils {
 
-    public static ByteArrayInputStream OpportunitiesToCSV(List<OpportunityDTO> opportunities) {
+    public static ByteArrayInputStream opportunitiesToCSV(List<OpportunityDTO> opportunities) {
         final CSVFormat format = CSVFormat.Builder.create().setHeader(HeaderEnum.class).build();
 
         ByteArrayOutputStream output = new ByteArrayOutputStream();
-        try (
-                CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(output), format);) {
+        try (CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(output), format)) {
             for (OpportunityDTO opps : opportunities) {
-                List<String> data = Arrays.asList(String.valueOf(opps.getProposalId()),
-                        String.valueOf(opps.getCustomer()),
-                        String.valueOf(opps.getPriceTonne()),
-                        String.valueOf(opps.getLastDollarQuotation()));
+                List<String> data = createCSVData(opps);
                 csvPrinter.printRecord(data);
             }
 
             csvPrinter.flush();
             return new ByteArrayInputStream(output.toByteArray());
         } catch (IOException e) {
-            throw new RuntimeException("Falha ao importar dados para CSV: " + e.getMessage());
+            throw new RuntimeException("Falha ao importar dados para CSV: " + e.getMessage(), e);
         }
+    }
+
+    private static List<String> createCSVData(OpportunityDTO opps) {
+        return Arrays.asList(String.valueOf(opps.getProposalId()),
+                String.valueOf(opps.getCustomer()),
+                String.valueOf(opps.getPriceTonne()),
+                String.valueOf(opps.getLastDollarQuotation()));
     }
 }
